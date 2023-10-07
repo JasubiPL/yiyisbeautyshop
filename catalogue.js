2db75eb4fab4940341f68dff4d72f7a6a10430df
JasubiPL <gameportal7@hotmail.com>
2023-10-07 10:30:41 -0600

    change all desing

--- catalogue.js
+++
@@ -1,7 +0,0 @@
-function redirect() {
-  setTimeout(() =>{
-    window.location.href = "https://catalogodigital.somosbelcorp.com/MX?consultant=MjcyNjYwMg=="
-  }, 1500)
-}
-
-redirect()
--- shop.js
+++
@@ -1,7 +0,0 @@
-function redirect() {
-  setTimeout(() =>{
-    window.location.href = "https://lbel.tiendabelcorp.com.mx/consultora/angelesjimenez1"
-  }, 1500)
-}
-
-redirect()
